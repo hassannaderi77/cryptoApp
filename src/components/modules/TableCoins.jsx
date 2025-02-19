9c1f343c8b2b4d6420d6bcb689c1997d24eba94f
hasanNaderi <hasannaderi27@gmail.com>
2025-02-19 16:33:28 +0330

    first commit

---
+++ src/components/modules/TableCoins.jsx
@@ -0,0 +1,34 @@
+import { RotatingLines } from "react-loader-spinner";
+import TableRow from "./TableRow";
+
+import styles from "../modules/TableCoins.module.css";
+
+function TableCoins({ coins, isLoading, setChart }) {
+  return (
+    <div className={styles.container}>
+      {isLoading ? (
+        <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} setChart={setChart} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default TableCoins;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://api.coingecko.com/api/v3"
+const API_KEY = "CG-RSkoD3vpSPARgxB4t8j9pvo1"
+
+
+const getCoinList = (page, currency) => {
+    return `${BASE_URL}/coins/markets?vs_currency=${currency}&order=market_cap-desc&per_page=20&page=${page}&x_cg_demo_api_key=${API_KEY}`
+}
+
+const searchCoin = (query) => {
+    return `${BASE_URL}/search?query=${query}&x_cg_demo_api_key=${API_KEY}`
+}
+
+const marketChart = (coin) => {
+    return `${BASE_URL}/coins/${coin}/market_chart?vs_currency=usd&days=7`
+}
+
+export {getCoinList, searchCoin, marketChart}
